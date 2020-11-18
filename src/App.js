8447755a4efb86a2d5e931525aad130677f141f3
Justin Langlinais <justinclanglinais@gmail.com>
2020-11-18 01:00:08 -0800

    feat(App): setup fake json

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import StudentList from './src/components/StudentList'
+import StudentList from './components/StudentList'
 
 function App() {
   return (
--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
-import StudentCard from './src/components/StudentCard.js'
+import StudentCard from './StudentCard.js'
+
+let studentsAPI = [
+    {
+        name: "Justin"
+    },
+    {
+        name: "Lucy"
+    },
+    {
+        name: "Potato"
+    },
+    {
+        name: "Otto"
+    }
+]
 
 class StudentList extends Component {
     render () {
         let { students } = this.props
         function renderStudents() {
-            students.map( student => {
+            studentsAPI.map( student => {
                 return (
                     <div>
                         <StudentCard student={student} />
